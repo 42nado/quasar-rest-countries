4f885e0c8f82f629ff99b90bbe9883a058372c54
Ironsick_ <daryllfortunado@gmail.com>
2023-04-12 16:59:36 +0800

    fetching data from the countries api

--- src/pages/AllCountries.vue
+++ src/pages/AllCountries.vue
@@ -1,7 +1,64 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="grid items-center">
     <h5>All Countries</h5>
+    <div class="flex country-cards row justify-around">
+      <q-card
+        v-for="country in countries"
+        :key="country.name.common"
+        class="my-card"
+      >
+        <q-img :src="getCountryDetails(country).flag" />
+        <q-card-section>
+          <div class="text-h6">{{ getCountryDetails(country).name }}</div>
+          <div class="text-subtitle2">
+            Population: {{ getCountryDetails(country).population }}
+          </div>
+          <div class="text-subtitle2">
+            Region: {{ getCountryDetails(country).region }}
+          </div>
+          <div class="text-subtitle2">
+            Capital: {{ getCountryDetails(country).capital }}
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
-<script></script>
+<script>
+import { ref } from "vue";
+import axios from "axios";
+
+export default {
+  setup() {
+    let countries = ref([]);
+    // create a function to fetch the data from the API and store it in the countries variable
+    const fetchCountries = async () => {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      countries.value = response.data;
+      console.log(countries.value);
+    };
+    // call the function
+    fetchCountries();
+
+    // function to get the flag image and country name population, region and capital from the countries variable
+    const getCountryDetails = (country) => {
+      return {
+        flag: country.flags.png,
+        name: country.name.common,
+        population: country.population,
+        region: country.region,
+        capital: country.capital,
+      };
+    };
+
+    return { countries, getCountryDetails };
+  },
+};
+</script>
+<style lang="sass" scoped>
+.my-card
+  width: 200px
+  max-width: 250px
+  margin: 10px
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,30 +1,61 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-input
-      outlined
-      bottom-slots
-      v-model="text"
-      label="Label"
-      counter
-      maxlength="12"
-      :dense="dense"
-    >
-      <template v-slot:before>
-        <q-icon name="flight_takeoff" />
-      </template>
+  <q-page class="row justify-around">
+    <div>
+      <q-input
+        class="search"
+        outlined
+        bottom-slots
+        v-model="text"
+        label="Search Country"
+      >
+        <template v-slot:before>
+          <q-icon name="flight_takeoff" />
+        </template>
+        <template v-slot:append>
+          <q-icon
+            v-if="text !== ''"
+            name="close"
+            @click="text = ''"
+            class="cursor-pointer"
+          />
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </div>
+    <div class="q-pa-md filter">
+      <q-btn-dropdown color="primary" label="Filter By Region">
+        <q-list>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Africa</q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <template v-slot:append>
-        <q-icon
-          v-if="text !== ''"
-          name="close"
-          @click="text = ''"
-          class="cursor-pointer"
-        />
-        <q-icon name="search" />
-      </template>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>America</q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <template v-slot:hint> Field hint </template>
-    </q-input>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Asia</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Europe</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Oceania</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
+    <div class="country"></div>
   </q-page>
 </template>
 
@@ -35,3 +66,9 @@ export default defineComponent({
   name: "IndexPage",
 });
 </script>
+
+<style>
+.q-page {
+  padding-top: 15px;
+}
+</style>
