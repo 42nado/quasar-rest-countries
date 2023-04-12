c5cbb37f1e35f9eb77c4d6dd3c3fcecaf6e7bc41
Ironsick_ <daryllfortunado@gmail.com>
2023-04-12 13:01:32 +0800

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input
+      outlined
+      bottom-slots
+      v-model="text"
+      label="Label"
+      counter
+      maxlength="12"
+      :dense="dense"
+    >
+      <template v-slot:before>
+        <q-icon name="flight_takeoff" />
+      </template>
+
+      <template v-slot:append>
+        <q-icon
+          v-if="text !== ''"
+          name="close"
+          @click="text = ''"
+          class="cursor-pointer"
+        />
+        <q-icon name="search" />
+      </template>
+
+      <template v-slot:hint> Field hint </template>
+    </q-input>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
